4401cd474000e8d21ff986786a7e8ee9f23585d0
jcabrera37 <56944675+jcabrera37@users.noreply.github.com>
2021-06-26 19:12:12 -0600

    initial commit
    first commit

---
+++ Portafolio/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link href="css/style.css"rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <form action="" class="form">
+            <div class="logo-img"><img class="logo-img" src="img/logo.jpg" alt="logoSerprotec.." width="200" heigth="200"></div>
+            <br/><br/>
+            <i class="fa fa-user"></i><input type="text" class="field" placeholder="user"><br/><br/>
+            
+            <i class="fa fa-unlock"></i><input class="field" type="password" placeholder="password"><br/><br>
+            <input class="btn-send" type="submit" value="Ingresar"><br/><br><br>
+            <a href="" ><span>Registrarse</span></a>
+        </form>
+    </div>
+</body>
+</html>
